9f49dad2e9d36b8232855cb93f37e0788e2c2838
gabiwassan <gabriel.wassan@wazuh.com>
2021-10-22 13:00:48 -0300

    feat(login): Added error components.

---
+++ client/src/components/layout/LoginError.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const QueLeDoyError = () => {
+  return (
+    <img src="https://c.tenor.com/SBAYO8dpFa8AAAAC/fidel-queledoy.gif" alt="que le doy?" width="250"/>
+  )
+}
+
+export const NoPhoneError = () => {
+  return (
+    <img
+      src="https://thumbs.gfycat.com/IndelibleElderlyChickadee-size_restricted.gif"
+      alt="no phone?" width="250"/>
+  )
+}
