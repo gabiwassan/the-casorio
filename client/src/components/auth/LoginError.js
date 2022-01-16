f60ff026dd76c79d96907a14549ebbf501aabd29
gabiwassan <gabriel.wassan@wazuh.com>
2022-01-16 17:58:54 -0300

    Ardillas

--- client/src/components/auth/LoginError.js
+++ client/src/components/auth/LoginError.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import ardilla from './../../assets/invalid-phone.jpg'
+import noPhone from './../../assets/no-phone.jpg'
+import './Login.css'
 
 export const QueLeDoyError = () => {
   return (
     <div className='container'>
-      <div className='col s1'/>
-      <div className='col s5'>
-        <img className='error-image' src='https://c.tenor.com/SBAYO8dpFa8AAAAC/fidel-queledoy.gif'
-             alt='que le doy?'/>
+      <div className='col s12'>
+        <img className='ardilla' src={noPhone}
+             alt='noPhone?'/>
       </div>
-      <div className='col s5'> Parece que tu <b>teléfono</b> no figura en la lista de <b>invitados</b>
-        , porfa comunicate conmigo <a href='https://wa.me/+543513079896'><b>aca</b></a></div>
-      <div className='col s1'/>
+      <div className='col s12'> Tú <b>teléfono</b> no figura en la lista de <b>invitados</b>, podes comunicate conmigo <a href='https://wa.me/+543517654165'><b>acá</b></a></div>
     </div>
 
   )
@@ -18,9 +18,9 @@ export const QueLeDoyError = () => {
 
 export const NoPhoneError = () => {
   return (
-    <div className='container'>
-      <div className='col s6 offset-s1'>
-        <img className='error-image' src='https://c.tenor.com/ampW8g8v-vMAAAAd/hack-khaby.gif' alt='really?'/>
+    <div className='container center-align'>
+      <div className='col s12'>
+        <img className='ardilla' src={ardilla} alt='really?'/>
       </div>
     </div>
   )
